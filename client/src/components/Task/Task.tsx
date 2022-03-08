67dd78c4096963bc98db8326227ae9b35d2317e6
Henry Nguyen <huyhung1601@gmail.com>
2022-03-09 09:07:02 +1100

    fix highlighted-task feature

--- client/src/components/Task/Task.tsx
+++ client/src/components/Task/Task.tsx
@@ -7,17 +7,23 @@ const Task = (props: any) => {
     /**MUI style */
     const classes = useStyle()
     const {task, prev, next} = classes
-    const compareDate =(date: string) =>{
+    const prevTime =(date: string) =>{
       const tday = new Date().getTime()      
       const time = new Date(date).getTime()
       return (time - tday)/(1000*24*3600)
+    }
+    const nextTime = (date: string) =>{
+      const tday = new Date().getTime()
+      const time = new Date(date).getTime() - (new Date(date).getHours()*3600 + new Date(date).getMinutes()*60 + new Date(date).getSeconds())*1000
+      return (time - tday)/(1000*24*3600)
+
     }
   return (
     <Draggable key={t.id} index={index} draggableId={t.id}>
       {(provided) => {
         return (
           <div
-            className={clsx(task,{[prev]:compareDate(t.start) <= -1},{[next]:compareDate(t.start) >=0} )}
+            className={clsx(task,{[prev]:prevTime(t.start) <= 0},{[next]:nextTime(t.start) >=0 } )}
             ref={provided.innerRef}
             {...provided.dragHandleProps}
             {...provided.draggableProps}
